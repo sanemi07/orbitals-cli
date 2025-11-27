dc2c4ee63a30d8c3efa4ff573593ec186fda3e02
sanemi07 <arjuntripathi1724@gmail.com>
2025-11-27 16:11:59 +0530

    initialisation

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app=express();
+const PORT=process.env.PORT || 3000;
+app.get('/',(req,res)=>{
+    res.send('hello world   from express server');
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
